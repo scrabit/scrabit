1653f17c33e27a0a8ddedc4a0bc3ac3151a82675
Watson Dinh <mr.kurro@gmail.com>
2025-01-10 14:08:32 -0600

    use deterministic session key for testing

--- examples/tic-tac-toe/server/src/main/scala/tictactoe/auth/Authenticator.scala
+++ examples/tic-tac-toe/server/src/main/scala/tictactoe/auth/Authenticator.scala
@@ -2,15 +2,18 @@ package tictactoe.auth
 
 import io.scrabit.actor.session.AuthenticationService.Login
 import org.apache.pekko.actor.typed.Behavior
-import org.apache.pekko.actor.typed.javadsl.Behaviors
+import org.apache.pekko.actor.typed.scaladsl.Behaviors
 import io.scrabit.actor.session.AuthenticationService
 import io.scrabit.actor.CommunicationHub.SessionCreated
 
 object Authenticator:
   def acceptAll: Behavior[Login] =
-    Behaviors.receiveMessage { case Login(userId, password, connection, replyTo) =>
-      println(s"user ${userId} logged in")
-      val newSessionKey = AuthenticationService.generateSessionKey()
-      replyTo ! SessionCreated(userId, newSessionKey, connection)
-      Behaviors.same
-    }
+    Behaviors.setup(context =>
+      Behaviors.receiveMessage { case Login(userId, password, connection, replyTo) =>
+        // val newSessionKey = AuthenticationService.generateSessionKey()
+        val newSessionKey = s"testkey-$userId"
+        context.log.info(s"user ${userId} logged in - generated session key $newSessionKey")
+        replyTo ! SessionCreated(userId, newSessionKey, connection)
+        Behaviors.same
+      }
+    )
